3c9b77baf22be860526d80163561c49f6e13297a
diegogqcr <diego.gamboa.cr@gmail.com>
2023-05-01 17:21:59 -0600

    create app calculadora

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import logoDGQ from './images/dgq.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='logo-contenedor'>
+        <img 
+          src={logoDGQ}
+          className='logo'
+          alt='Logo de DGGQ'
+        />
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+       
+      </div>  
     </div>
   );
 }
